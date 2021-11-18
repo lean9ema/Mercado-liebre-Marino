e99f9a2c2003118423f085e98d925407193b36e9
lean9ema <lean9marino@gmail.com>
2021-11-18 10:56:16 -0300

    Lean- Agregue el html  login y register

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(3000, () => {
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/home.html');//cambia el path.resolve()
 })
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
