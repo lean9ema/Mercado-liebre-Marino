88221b52cf44e7f94618ce1dfb15f931c155296a
lean9ema <lean9marino@gmail.com>
2021-11-23 11:15:06 -0300

    Lean- cambie la ruta de home en el logo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor funcionando')
 })
 
-app.get('/home', (req,res) => {
+app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/home.html');//cambia el path.resolve()
 })
 
